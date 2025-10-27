20dbef5f7b70ecf27347290e3278ee663b82b3b1
Oscar <6631501194@lamduan.mfu.ac.th>
2025-10-27 22:50:18 +0700

    student browse list

---
+++ lib/student_BrowseList.dart
@@ -0,0 +1,317 @@
+import 'package:flutter/material.dart';
+
+class StudentBrowse extends StatefulWidget {
+  const StudentBrowse({super.key});
+
+  @override
+  State<StudentBrowse> createState() => _StudentBrowseState();
+}
+
+class _StudentBrowseState extends State<StudentBrowse> {
+  int _selectedIndex = 0;
+
+  final List<Map<String, String>> rooms = [
+    {
+      "name": "Room 1",
+      "type": "Meeting room",
+      "image": "assets/images/room1.png",
+    },
+    {
+      "name": "Room 2",
+      "type": "Meeting room",
+      "image": "assets/images/room1.png",
+    },
+    {
+      "name": "Room 3",
+      "type": "Seminar room",
+      "image": "assets/images/room1.png",
+    },
+    {
+      "name": "Room 4",
+      "type": "Study room",
+      "image": "assets/images/room1.png",
+    },
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+   
+    return Scaffold(
+      backgroundColor: Colors.white,
+
+    
+      body: SafeArea(
+        child: Column(
+          children: [
+            // Header (fixed)
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 16.0,
+                vertical: 14,
+              ),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  // Greeting
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: const [
+                        
+                        Text(
+                          "Hello, Oscar",
+                          style: TextStyle(
+                            fontSize: 34,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                            shadows: [
+                              Shadow(
+                                offset: Offset(2, 4),
+                                blurRadius: 6,
+                                color: Color.fromARGB(40, 0, 0, 0),
+                              ),
+                            ],
+                          ),
+                        ),
+
+                        Text(
+                          "Welcome to Room Reservation",
+                          style: TextStyle(
+                            fontSize: 25,
+                            color: Colors.black87,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+
+                  // Logout (fixed)
+                  Column(
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                          color: const Color(0xFFB9D6FF),
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        child: IconButton(
+                          icon: const Icon(
+                            Icons.logout,
+                            color: Color.fromARGB(255, 4, 57, 147),
+                          ),
+                          onPressed: () {},
+                        ),
+                      ),
+                      const SizedBox(height: 6),
+                      const Text(
+                        "Logout",
+                        style: TextStyle(
+                          color: Color.fromARGB(255, 21, 97, 228),
+                          fontSize: 13,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+
+            
+            Expanded(
+              child: Container(
+               
+                decoration: const BoxDecoration(
+                  color: Color(0xFFB9D6FF),
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(40),
+                    topRight: Radius.circular(40),
+                  ),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(16, 18, 16, 0),
+                  child: SingleChildScrollView(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        // Search bar
+                        Container(
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                          child: const TextField(
+                            decoration: InputDecoration(
+                              prefixIcon: Icon(Icons.search),
+                              hintText: 'Room',
+                              border: InputBorder.none,
+                              contentPadding: EdgeInsets.symmetric(
+                                vertical: 15,
+                              ),
+                            ),
+                          ),
+                        ),
+
+                        const SizedBox(height: 20),
+
+                        // Room list
+                        Center(
+                          child: Column(
+                            children: rooms.map((room) {
+                              return Container(
+                                width: 350,
+                               
+                                padding: const EdgeInsets.all(12),
+                                margin: const EdgeInsets.only(bottom: 18),
+                                decoration: BoxDecoration(
+                                  color: const Color(0xFFEFF4FF),
+                                  borderRadius: BorderRadius.circular(16),
+                                  boxShadow: [
+                                    BoxShadow(
+                                      color: Colors.black12,
+                                      blurRadius: 6,
+                                      offset: const Offset(2, 2),
+                                    ),
+                                  ],
+                                ),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Row(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.center,
+                                      children: [
+                                        // Room Image
+                                        ClipRRect(
+                                          borderRadius: BorderRadius.circular(
+                                            8,
+                                          ),
+                                          child: Image.asset(
+                                            room["image"]!,
+                                            width: 200,
+                                            height: 100,
+                                            fit: BoxFit.contain,
+                                            errorBuilder:
+                                                (context, error, stackTrace) {
+                                                  return Container(
+                                                    width: 140,
+                                                    height: 100,
+                                                    color: Colors.grey[300],
+                                                    child: const Icon(
+                                                      Icons.image_not_supported,
+                                                      color: Colors.grey,
+                                                      size: 30,
+                                                    ),
+                                                  );
+                                                },
+                                          ),
+                                        ),
+                                        const SizedBox(width: 16),
+
+                                        // Detail button aligned to right
+                                        Align(
+                                          alignment: Alignment.centerRight,
+                                          child: ElevatedButton.icon(
+                                            onPressed: () {},
+                                            icon: const Icon(
+                                              Icons.info_outline,
+                                              size: 20,
+                                              color: Colors.white,
+                                            ),
+                                            label: const Text(
+                                              "Detail",
+                                              style: TextStyle(
+                                                fontSize: 14,
+                                                fontWeight: FontWeight.bold,
+                                                color: Colors.white,
+                                              ),
+                                            ),
+
+                                            style: ElevatedButton.styleFrom(
+                                              backgroundColor: const Color(
+                                                0xFF222558,
+                                              ),
+
+                                              foregroundColor: Colors.white,
+                                              padding:
+                                                  const EdgeInsets.symmetric(
+                                                    horizontal: 18,
+                                                    vertical: 12,
+                                                  ),
+                                              shape: RoundedRectangleBorder(
+                                                borderRadius:
+                                                    BorderRadius.circular(20),
+                                              ),
+                                            ),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                    const SizedBox(height: 12),
+
+                                   
+                                    Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.center,
+                                         
+                                      children: [
+                                        Text(
+                                          
+                                          room["name"]!,
+                                          style: const TextStyle(
+                                            fontSize: 22,
+                                            fontWeight: FontWeight.bold,
+                                            color: Colors.black,
+                                          ),
+                                        ),
+                                        const SizedBox(height: 4),
+                                        Text(
+                                          "       (${room["type"]!})",
+                                          style: const TextStyle(
+                                            fontSize: 18,
+                                            color: Colors.black87,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                              );
+                            }).toList(),
+                          ),
+                        ),
+
+                        const SizedBox(height: 24),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+
+     
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        selectedItemColor: const Color(0xFF1E3A8A),
+        unselectedItemColor: Colors.grey,
+        onTap: _onItemTapped,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.list_alt),
+            label: "Check Requests",
+          ),
+          BottomNavigationBarItem(icon: Icon(Icons.history), label: "History"),
+        ],
+      ),
+    );
+  }
+}
